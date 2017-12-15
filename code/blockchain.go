25fb5cb5c970420e93f1ad033604e827727dcb1f
Melkior <melkiorthambipillai@gmail.com>
2017-12-16 00:33:12 +0100

    add basic functions for blocks

--- code/blockchain.go
+++ code/blockchain.go
@@ -5,10 +5,17 @@ import(
 	"crypto/sha256"
 	"time"
 	"encoding/binary"
+	"bytes"
+	"fmt"
+	"math"
 )
 
-type BlockChain struct{
+const difficulty = 22
+const expiration = time.Second*30
 
+type BlockChain struct{
+	BlocksPerNodeID map[uint64]*Block
+	LastBlock *Block
 }
 
 type Block struct{
@@ -36,8 +43,80 @@ func (b *Block) getBytes()[]byte{
     return append(nodeTimePubKeyNonce, b.PreviousHash...)
 }
 
-func (b *Block) hash()[]byte{
+func (b *Block) hash() []byte{
     hasher := sha256.New()
 	hasher.Write(b.getBytes())
 	return hasher.Sum(nil)
+}
+
+func (bc *BlockChain) addBlock(b *Block) bool{
+	isValid := validateBlock(b)
+	matchesLast := false
+	if(bc.LastBlock==nil){
+		matchesLast = true
+	}else{
+		matchesLast = bytes.Compare(b.PreviousHash, bc.LastBlock.hash())==0
+	}
+	if(isValid && matchesLast){
+		bc.BlocksPerNodeID[b.NodeID] = b
+		bc.LastBlock = b
+		return true
+	}else{
+		return false
+	}
+}
+
+func (bc *BlockChain) containsNodeID(id uint64) bool{
+	_,ok := bc.BlocksPerNodeID[id]
+	return ok
+}
+
+func (bc *BlockChain) getLastHash() []byte{
+	return bc.LastBlock.hash()
+}
+
+func (bc *BlockChain) containsBlock(b *Block) bool{
+	_,ok := bc.BlocksPerNodeID[b.NodeID]
+	return ok
+}
+
+func (bc *BlockChain) initGenesis(id uint64, pub rsa.PublicKey){
+	fmt.Println("start BlockChain")
+	bc.BlocksPerNodeID = make(map[uint64]*Block)
+	b := mineBlock(id,time.Now(),pub,nil)
+	bc.BlocksPerNodeID[id] = b
+	bc.LastBlock = b
+}
+
+func validateBlock(b *Block) bool{
+	nbZeroBytes := int(math.Ceil(difficulty/8.0))
+	h := b.hash()[0:nbZeroBytes]
+	for i:=0;i<nbZeroBytes-1;i=i+1{
+		if(h[i]!=0){
+			return false
+		}
+	}
+	remainingNbBits := difficulty - (nbZeroBytes-1)*8
+	lastByte := fmt.Sprintf("%08b",int64(h[nbZeroBytes-1]))
+	for i:=0;i<remainingNbBits;i=i+1{
+		if(string(lastByte[i])!="0"){
+			return false
+		}
+	}
+	return true
+}
+
+func mineBlock(id uint64, timestamp time.Time, pub rsa.PublicKey, previousHash []byte) *Block{
+	i := 0
+	trial := &Block{}
+	for{
+		nonceBytes := make([]byte, 8)
+		binary.LittleEndian.PutUint64(nonceBytes, uint64(i))
+		trial = &Block{id, timestamp, pub, nonceBytes, previousHash}
+		if(validateBlock(trial)){
+			return trial
+		}
+		i=i+1
+	}
+	return nil
 }

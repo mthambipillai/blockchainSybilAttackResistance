8fe8384ad77c576d1b8dd4d25411d548537f2773
Melkior <melkiorthambipillai@gmail.com>
2017-12-17 16:21:24 +0100

    add block time expiration control

--- code/blockchain.go
+++ code/blockchain.go
@@ -10,8 +10,8 @@ import(
 	"math"
 )
 
-const difficulty = 22
-const expiration = time.Second*30
+const difficulty = 18
+const expiration = time.Second*10
 
 type BlockChain struct{
 	BlocksPerNodeID map[uint64]*Block
@@ -49,15 +49,40 @@ func (b *Block) hash() []byte{
 	return hasher.Sum(nil)
 }
 
+func (b *Block) expired() bool{
+	elapsed := time.Since(b.Timestamp)
+    return elapsed.Nanoseconds() > expiration.Nanoseconds()
+}
+
+func (b *Block) isValid() bool{
+	if(b.expired()){
+		return false
+	}
+	nbZeroBytes := int(math.Ceil(difficulty/8.0))
+	h := b.hash()[0:nbZeroBytes]
+	for i:=0;i<nbZeroBytes-1;i=i+1{
+		if(h[i]!=0){
+			return false
+		}
+	}
+	remainingNbBits := difficulty - (nbZeroBytes-1)*8
+	lastByte := fmt.Sprintf("%08b",int64(h[nbZeroBytes-1]))
+	for i:=0;i<remainingNbBits;i=i+1{
+		if(string(lastByte[i])!="0"){
+			return false
+		}
+	}
+	return true
+}
+
 func (bc *BlockChain) addBlock(b *Block) bool{
-	isValid := validateBlock(b)
 	matchesLast := false
 	if(bc.LastBlock==nil){
 		matchesLast = true
 	}else{
 		matchesLast = bytes.Compare(b.PreviousHash, bc.LastBlock.hash())==0
 	}
-	if(isValid && matchesLast){
+	if(b.isValid() && matchesLast){
 		bc.BlocksPerNodeID[b.NodeID] = b
 		bc.LastBlock = b
 		return true
@@ -83,40 +108,29 @@ func (bc *BlockChain) containsBlock(b *Block) bool{
 func (bc *BlockChain) initGenesis(id uint64, pub rsa.PublicKey){
 	fmt.Println("start BlockChain")
 	bc.BlocksPerNodeID = make(map[uint64]*Block)
-	b := mineBlock(id,time.Now(),pub,nil)
+	var b *Block = nil
+	for b==nil{
+		b = mineBlock(id,time.Now(),pub,nil)
+	}
 	bc.BlocksPerNodeID[id] = b
 	bc.LastBlock = b
 }
 
-func validateBlock(b *Block) bool{
-	nbZeroBytes := int(math.Ceil(difficulty/8.0))
-	h := b.hash()[0:nbZeroBytes]
-	for i:=0;i<nbZeroBytes-1;i=i+1{
-		if(h[i]!=0){
-			return false
-		}
-	}
-	remainingNbBits := difficulty - (nbZeroBytes-1)*8
-	lastByte := fmt.Sprintf("%08b",int64(h[nbZeroBytes-1]))
-	for i:=0;i<remainingNbBits;i=i+1{
-		if(string(lastByte[i])!="0"){
-			return false
-		}
-	}
-	return true
-}
-
 func mineBlock(id uint64, timestamp time.Time, pub rsa.PublicKey, previousHash []byte) *Block{
 	i := 0
+	start := time.Now()
 	trial := &Block{}
 	for{
 		nonceBytes := make([]byte, 8)
 		binary.LittleEndian.PutUint64(nonceBytes, uint64(i))
 		trial = &Block{id, timestamp, pub, nonceBytes, previousHash}
-		if(validateBlock(trial)){
+		if(trial.isValid()){
 			return trial
 		}
 		i=i+1
+		if(time.Since(start).Nanoseconds() > expiration.Nanoseconds()){
+			return nil
+		}
 	}
 	return nil
 }

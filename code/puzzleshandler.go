60d68cb60acc16bbb1820d98947240300568d683
Melkior <melkiorthambipillai@gmail.com>
2017-12-17 16:38:34 +0100

    add handler basic structure for part 1

---
+++ code/puzzleshandler.go
@@ -0,0 +1,38 @@
+package main
+import(
+	"time"
+)
+type PuzzleProposal struct{
+	Origin			string
+	NodeID 			uint64
+	Timestamp 		time.Time
+	PreviousHash	[]byte
+}
+
+type PuzzleResponse struct{
+	Origin 			string
+	Destination		string
+	CreatedBlock	*Block
+}
+
+type BlockBroadcast struct{
+	Origin		string
+	NewBlock	*Block
+}
+
+type PuzzlesState struct{
+	LocalChain	BlockChain
+}
+
+
+func (ps *PuzzlesState) handlePuzzleProposal(pp *PuzzleProposal){
+
+}
+
+func (ps *PuzzlesState) handlePuzzleResponse(pp *PuzzleProposal){
+
+}
+
+func (ps *PuzzlesState) handleBlockBroadcast(pp *PuzzleProposal){
+
+}
